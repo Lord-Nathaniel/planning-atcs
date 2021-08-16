d404d69cb5e5513dd30905b3263c001562179e2f
Adrien Lozano <ad_lozano@outlook.com>
2021-08-16 20:30:31 +0200

    :memo: saving before new test with authentication

---
+++ pages/atelier-indexwo.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            message:'not logged in'
+        }
+    },
+    async mounted() {
+        const response = await fetch('http://localhost:8000/api/utilisateurs', {
+            header: { 'Content-Type':'application/json' },
+            credentials: 'include',
+        })
+        const content = await response.json();
+        
+        this.message = 'Hello' + content.name;
+    }
+}
+</script>
+<style>
+    
+</style>
--- pages/atelier-list.vue
+++ pages/atelier-list.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-    
+    middleware: ['auth-admin']
 }
 </script>
 <style>
